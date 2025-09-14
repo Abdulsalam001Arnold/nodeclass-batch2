f0aeb3c693c60383f8fb4c8a4c0721b97e3ea8f8
Abdulsalam001Arnold <abdulsalamabulhamidarnold@gmail.com>
2025-09-14 22:29:05 +0100

    updated

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,9 @@
 const { userValidator } = require("../validator/userValidator");
 const userModel = require("../models/userSchema");
+const bycrypt = require("bcryptjs");
+const {generateToken} = require('../middleware/generateToken')
 
-const postUser = (req, res) => {
+const postUser = async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
@@ -13,6 +15,11 @@ const postUser = (req, res) => {
       });
     }
 
+    const userExist = await userModel.findOne({ email });
+    if (userExist) {
+      return res.status(400).json({ message: "User already exists!!" });
+    }
+
     const user = userModel.create({
       username,
       email,
@@ -22,6 +29,7 @@ const postUser = (req, res) => {
     return res.status(201).json({
       message: "User created successfully",
       user,
+      token: generateToken((await user)._id)
     });
   } catch (err) {
     console.error(err);
@@ -31,4 +39,50 @@ const postUser = (req, res) => {
   }
 };
 
-module.exports = { postUser }
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const existingUser = await userModel.findOne({ email });
+
+    if (!existingUser)
+      return res
+        .status(400)
+        .json({ message: "User does not exist, please sign up." });
+
+    const checkPassword = await bycrypt.compare(
+      password,
+      existingUser.password
+    );
+
+    if (!checkPassword)
+      return res.status(400).json({ message: "Invalid Credentials" });
+
+    return res.status(200).json({
+      message: "User logged in",
+      existingUser,
+      token: generateToken(existingUser._id)
+    });
+  } catch (error) {
+    console.error(error)
+  }
+};
+
+const getAll = async(req, res) => {
+  try {
+    const users = await userModel.find()
+
+    if(!users) return res.status(404).json({
+      message: 'No users found.'
+    })
+
+    res.status(200).json(users)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({
+      reason: error.message
+    })
+  }
+}
+
+module.exports = { postUser, login, getAll };
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -2,8 +2,9 @@
 const express = require('express')
 const router = express.Router()
 const {getContact, postContact} = require('../controllers/contactController')
-const {postUser} = require('../controllers/userController')
+const {postUser, login, getAll} = require('../controllers/userController')
+const {protect} = require('../middleware/authMiddleware')
 
-router.get('/contact', getContact).post('/post-contact', postContact).post('/post-user', postUser)
+router.get('/contact', getContact).post('/post-contact', postContact).post('/post-user', postUser).post('/login', login).get('/all-users', protect, getAll)
 
 module.exports = router
